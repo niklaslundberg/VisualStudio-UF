ee8c12dd0f3dbfea5962a4dbed644998674cfadd
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-09-11 23:47:19 +0200

    Added schedule view with API data.

---
+++ Samples/WebApplication2/WebApplication2/Controllers/ScheduleController.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string json = httpClient.GetStringAsync("http://ufdemo.azurewebsites.net/schedule").Result;
+
+
+                List<ScheduleItem> schedule = JsonConvert.DeserializeObject<List<ScheduleItem>>(json);
+
+                return View(schedule);
+            }
+
+        }
+    }
+}
